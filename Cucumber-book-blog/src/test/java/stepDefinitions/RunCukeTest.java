ad63b38fc97c7801f79275b0758a4814d07defbb
nabeelrahman <nrahman@SDGL14db0b100.local>
2016-09-30 12:12:59 -0700

    Adding test cases:

---
+++ Cucumber-book-blog/src/test/java/stepDefinitions/RunCukeTest.java
@@ -0,0 +1,17 @@
+package stepDefinitions;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/features",
+		glue = "stepDefinitions",
+		plugin = { "pretty", "html:target/cucumber",
+				
+		})
+
+public class RunCukeTest {
+
+}
